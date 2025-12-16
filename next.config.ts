227177d46cdedf98311eebe6086ac79b728b40f3
sifna-ayisha <ayishasifnapk@gmail.com>
2025-12-16 17:30:43 +0530

    Fix Vercel build by removing local outputFileTracingRoot

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-  outputFileTracingRoot: "C:\\Users\\hp\\Documents\\sysbreeze\\my-next-app",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
